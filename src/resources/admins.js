fd2ef17355783e9e67ff0b34222f86eadc953da8
Marco Migliaro <marcomig96@gmail.com>
2022-05-10 02:14:33 -0300

    TG: improved responses in admins.js file

--- src/resources/admins.js
+++ src/resources/admins.js
@@ -2,26 +2,27 @@ const fs = require('fs');
 const admins = require('../data/admins.json');
 
 const getAllAdmins = (req, res) => {
-  res.status(200).json({
-    data: admins,
-  });
+  res.status(200).json({ data: admins });
 };
 
 const addAdmin = (req, res) => {
   const adminData = req.body;
-  if (adminData.id && adminData.name && adminData.lastName && adminData.email && adminData.password
-            && adminData.gender && adminData.phone && adminData.dateBirth && adminData.city
-            && adminData.zip && adminData.active) {
+  const admin = admins.find((item) => item.id === parseInt(adminData.id, 10));
+  if (admin) {
+    res.json({ msg: `The admin with ID ${adminData.id} already exists. Please use a new ID` });
+  } else if (adminData.id && adminData.name && adminData.lastName && adminData.email
+            && adminData.password && adminData.gender && adminData.phone && adminData.dateBirth
+            && adminData.city && adminData.zip && adminData.active) {
     admins.push(adminData);
     fs.writeFile('src/data/admins.json', JSON.stringify(admins), (error) => {
       if (error) {
         res.send(error);
       } else {
-        res.send(`Admin with ID ${adminData.id} was created`);
+        res.json({ msg: `Admin with ID ${adminData.id} was created` });
       }
     });
   } else {
-    res.send(`Admin with ID ${adminData.id} was not created. Please complete all fields`);
+    res.json({ msg: `Admin with ID ${adminData.id} was not created. Please complete all fields` });
   }
 };
 
@@ -59,13 +60,13 @@ const deleteAdminById = (req, res) => {
   const adminId = req.params.id;
   const filteredAdmins = admins.filter((item) => item.id !== parseInt(adminId, 10));
   if (admins.length === filteredAdmins.length) {
-    res.send(`Could not delete admin with ID ${adminId} because it was not found`);
+    res.json({ msg: `Could not delete admin with ID ${adminId} because it was not found` });
   } else {
     fs.writeFile('src/data/admins.json', JSON.stringify(filteredAdmins), (error) => {
       if (error) {
         res.send(error);
       } else {
-        res.send(`Admin with ID ${adminId} was deleted`);
+        res.json({ msg: `Admin with ID ${adminId} was deleted` });
       }
     });
   }
@@ -76,9 +77,9 @@ const getAdminByGender = (req, res) => {
   const adminGender = gender.substring(0, 1).toUpperCase() + gender.substring(1).toLowerCase();
   const filteredAdmins = admins.filter((item) => item.gender === adminGender);
   if (filteredAdmins.length > 0) {
-    res.send(filteredAdmins);
+    res.json({ data: filteredAdmins });
   } else {
-    res.send(`There are no admins with gender: ${adminGender}`);
+    res.json({ msg: `There are no admins with gender: ${adminGender}` });
   }
 };
 
@@ -87,9 +88,9 @@ const getAdminByName = (req, res) => {
   const adminName = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
   const filteredAdmins = admins.filter((item) => item.name === adminName);
   if (filteredAdmins.length > 0) {
-    res.send(filteredAdmins);
+    res.json({ data: filteredAdmins });
   } else {
-    res.send(`There are no admins with the name: ${adminName}`);
+    res.json({ msg: `There are no admins with the name: ${adminName}` });
   }
 };
 
