4d8a6be7240bba8fd3a2edf0e09e0070a1009092
Nicolas Lalicata <nicolaughing@gmail.com>
2022-05-09 22:37:59 -0300

    TG: Finished search element by id and create user

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 // use "import" to import libraries
 import express from 'express';
-
+import { urlencoded } from 'express';
+import * as employeesControllers from './resources/employees';
 // use "require" to import JSON files
-const admins = require('./data/admins.json');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
 app.use(express.json());
+
+
+app.get('/employees', employeesControllers.getAllEmployee);
+app.get('/employees/:id', employeesControllers.getOnlyId);
+app.get('/employees/:phone', employeesControllers.getOnlyId);
+app.post('/employees', employeesControllers.createMember);
 
 app.get('/', async (req, res) => {
   res.send('Hello World!');
