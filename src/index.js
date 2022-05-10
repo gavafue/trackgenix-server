911174a071baafed99d13cce2561ee585ab4ff8b
AlejandroSantini <alejandro.j.santini@gmail.com>
2022-05-10 19:42:46 -0300

    TG: Delete comments

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import express from 'express';
 import * as employeesControllers from './resources/employees';
 
 // use "require" to import JSON files
-// const admins = require('./data/admins.json');
-
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
@@ -16,12 +14,6 @@ app.get('/employee/:lastName', employeesControllers.filterByLastName);
 app.get('/', async (req, res) => {
   res.send('Hello !');
 });
-
-/* app.get('/admins', (req, res) => {
-  res.status(200).json({
-    data: admins,
-  });
-}); */
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
