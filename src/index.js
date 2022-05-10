3285bca96bc2e0c12fa8e5593c6a5d86047b7983
Nicolas Lalicata <nicolaughing@gmail.com>
2022-05-10 15:42:13 -0300

    TG: Filter controller finished, errors corrections in controllers and ready to deliver

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 // use "import" to import libraries
 import express from 'express';
-import { urlencoded } from 'express';
 import * as employeesControllers from './resources/employees';
 // use "require" to import JSON files
 
@@ -9,20 +8,13 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-
 app.get('/employees', employeesControllers.getAllEmployee);
 app.get('/employees/:id', employeesControllers.getOnlyId);
-app.get('/employees/:phone', employeesControllers.getOnlyId);
+app.get('/employees/country/:country', employeesControllers.filterByCountry);
 app.post('/employees', employeesControllers.createMember);
 
 app.get('/', async (req, res) => {
   res.send('Hello World!');
-});
-
-app.get('/admins', (req, res) => {
-  res.status(200).json({
-    data: admins,
-  });
 });
 
 app.listen(port, () => {
--- src/resources/employees.js
+++ src/resources/employees.js
@@ -1,52 +1,58 @@
-import res, { append } from 'express/lib/response';
+const fs = require('file-system');
 const employees = require('../data/employees.json');
-const fs = require ('file-system')
 
-const getAllEmployee = (req,res) => {
-    res.status(200).json({
-        data: employees,
-    })
+const getAllEmployee = (req, res) => {
+  res.status(200).json({
+    data: employees,
+  });
 };
 
-const getOnlyId = (req,res) => {
-    const errors = employees.some (user => user.id === parseInt(req.params.id))
-    if (errors){
-        res.json(employees.filter(user => user.id === parseInt(req.params.id)));
-    } else {
-        res.status(400).json ({msg: `Member with id:${req.params.id} not found`});
-    }};
+const getOnlyId = (req, res) => {
+  const errors = employees.some((user) => user.id === parseInt(req.params.id, 10));
+  if (errors) {
+    res.json(employees.filter((user) => user.id === parseInt(req.params.id, 10)));
+  } else {
+    res.status(400).json({ msg: `Member with id:${req.params.id} not found` });
+  }
+};
 
-const createMember = (req,res) => {
-    const newMember = {
-            id: req.body.id,
-            firstName: req.body.firstName,
-            lastName: req.body.lastName,
-            birthDate: req.body.birthDate,
-            country: req.body.country,
-            city: req.body.city,
-            zip: req.body.zip,
-            phone: req.body.phone,
-            email: req.body.email,
-            password: req.body.password,
-            photo: req.body.photo,
-            active: req.body.active,
-                     }
-    if (!req.body.id || !req.body.firstName || !req.body.lastName || !req.body.birthDate ||
-        !req.body.country || !req.body.city || !req.body.zip || !req.body.phone ||
-        !req.body.email || !req.body.password || !req.body.photo || !req.body.active  ){
-           return res.status(400).json({msg: 'Please fill in the fields correctly', newMember})
-    
-    } else {
-        employees.push(newMember);
-        fs.writeFile('./src/data/employees.json', JSON.stringify(employees));
-        res.status(200).json({msg:'User created successfully', newMember})
-    }
-    };
-    
+const createMember = (req, res) => {
+  const newMember = {
+    id: req.body.id,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    birthDate: req.body.birthDate,
+    country: req.body.country,
+    city: req.body.city,
+    zip: req.body.zip,
+    phone: req.body.phone,
+    email: req.body.email,
+    password: req.body.password,
+    photo: req.body.photo,
+    active: req.body.active,
+  };
+  if (!req.body.id || !req.body.firstName || !req.body.lastName || !req.body.birthDate
+        || !req.body.country || !req.body.city || !req.body.zip || !req.body.phone
+        || !req.body.email || !req.body.password || !req.body.photo || !req.body.active) {
+    return res.status(400).json({ msg: 'Please fill in the fields correctly', newMember });
+  }
+  employees.push(newMember);
+  fs.writeFile('./src/data/employees.json', JSON.stringify(employees));
+  return res.status(200).json({ msg: 'User created successfully', newMember });
+};
+
+const filterByCountry = (req, res) => {
+  const errors2 = employees.some((user) => user.country === req.params.country);
+  if (errors2) {
+    res.json(employees.filter((user) => user.country === req.params.country));
+  } else {
+    res.status(400).json({ msg: `Member with nationality: ${req.params.country} not found` });
+  }
+};
 
 export {
-    getAllEmployee,
-    getOnlyId,
-    createMember,
-    
-}
+  getAllEmployee,
+  getOnlyId,
+  createMember,
+  filterByCountry,
+};
