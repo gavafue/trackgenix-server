34a9d509413bec9ffedfcaae910c02b803ac5e0c
Marco Migliaro <marcomig96@gmail.com>
2022-05-10 14:45:22 -0300

    TG: deleted comments in admin.js file

--- src/index.js
+++ src/index.js
@@ -3,22 +3,9 @@ import express from 'express';
 import * as adminsControllers from './resources/admins';
 import * as timeSheetControllers from './resources/time-sheets';
 
-// use "require" to import JSON files
-// const admins = require('./data/admins.json');
-
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
-
-// Routes admins
-
-app.get('/admins', adminsControllers.getAllAdmins);
-app.post('/admins/add', adminsControllers.addAdmin);
-app.put('/admins/update/:id', adminsControllers.updateAdmin);
-app.get('/admins/getById/:id', adminsControllers.getAdminById);
-app.delete('/admins/deleteById/:id', adminsControllers.deleteAdminById);
-app.get('/admins/getByGender', adminsControllers.getAdminByGender);
-app.get('/admins/getByName', adminsControllers.getAdminByName);
 
 app.get('/', async (req, res) => {
   res.send('Hello World!');
@@ -28,6 +15,15 @@ app.get('/', async (req, res) => {
 app.post('/timeSheet', timeSheetControllers.createTimeSheet);
 app.delete('/timeSheet/:id', timeSheetControllers.deleteTimeSheet);
 app.get('/timeSheet/:employeeName', timeSheetControllers.filterTSByName);
+
+// Routes admins
+app.get('/admins', adminsControllers.getAllAdmins);
+app.post('/admins/add', adminsControllers.addAdmin);
+app.put('/admins/update/:id', adminsControllers.updateAdmin);
+app.get('/admins/getById/:id', adminsControllers.getAdminById);
+app.delete('/admins/deleteById/:id', adminsControllers.deleteAdminById);
+app.get('/admins/getByGender', adminsControllers.getAdminByGender);
+app.get('/admins/getByName', adminsControllers.getAdminByName);
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
