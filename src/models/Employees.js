834c43e99f057a3215b56af42240ccac5f6d1a66
Alexis Suarez <alexsuarezproyecto@gmail.com>
2022-05-13 14:51:22 -0300

    TG-06: Created the schema for employee

---
+++ src/models/Employees.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const employeeSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  birthDate: {
+    type: Date,
+    required: true,
+  },
+  country: {
+    type: String,
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  zip: {
+    type: Number,
+    required: true,
+  },
+  phone: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  photo: {
+    type: URL,
+    required: true,
+  },
+  active: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+const Employees = mongoose.model('Employee', employeeSchema);
+module.exports = Employees;
