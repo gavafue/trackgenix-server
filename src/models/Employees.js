995cfc6f6f873da8a64622349890a55888fa5a3d
Alexis Suarez <alexsuarezproyecto@gmail.com>
2022-05-13 19:53:35 -0300

    TG-06: changed ES5 (require and module.exports) for ES6 (import and export)

--- src/models/Employees.js
+++ src/models/Employees.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const employeeSchema = new mongoose.Schema({
   firstName: {
@@ -48,4 +48,4 @@ const employeeSchema = new mongoose.Schema({
 });
 
 const Employees = mongoose.model('Employee', employeeSchema);
-module.exports = Employees;
+export default Employees;
