c721dd58338491f980776d054de572ffbc9a142f
AlejandroSantini <alejandro.j.santini@gmail.com>
2022-05-15 22:26:56 -0300

    TG-19: Add createTask

--- src/controllers/tasks.js
+++ src/controllers/tasks.js
@@ -1,107 +1,103 @@
-const fs = require('fs');
-const taskData = require('../data/tasks.json');
+import models from '../models/Tasks';
 
-const elements = ['nameProject', 'week', 'day', 'description', 'hours'];
-
-const createTask = (req, res) => {
-  const newTask = req.body;
-  const validateElements = elements.every((item) => Object.keys(newTask).includes(item));
-
-  newTask.id = Math.floor(Math.random() * 10000);
-
-  const foundID = taskData.some((task) => task.id === newTask.id);
-
-  if (validateElements && !foundID) {
-    taskData.push(newTask);
-    fs.writeFile('src/data/tasks.json', JSON.stringify(taskData), (err) => {
-      if (err) {
-        res.send(err);
-      } else {
-        res.status(200).json({
-          msg: 'Task created',
+const createTask = async(req, res) => {
+    try {
+        const task = new models.Task({
+            nameProject: req.body.nameProject,
+            week: req.body.week,
+            day: req.body.day,
+            description: req.body.description,
+            hours: req.body.hours,
         });
-      }
-    });
-  } else {
-    res.status(400).json({
-      msg: 'Task could not be created',
-    });
-  }
-};
-
-const editTask = (req, res) => {
-  const { id } = req.params;
-  const task = taskData.find((item) => item.id === parseInt(id, 10));
-
-  if (!task) {
-    res.status(200).json({
-      msg: `The task with ID ${id} does not exist`,
-    });
-  } else {
-    const updTask = req.body;
-    elements.forEach((prop) => {
-      task[prop] = updTask[prop] ? updTask[prop] : task[prop];
-    });
-    fs.writeFile('src/data/tasks.json', JSON.stringify(taskData), (err) => {
-      if (err) {
-        res.send(err);
-      } else {
-        res.status(200).json({
-          msg: 'The task was updated',
+        const result = await task.save();
+        return res.status(200).json({
+            message: 'Task created successfully',
+            result,
         });
-      }
-    });
-  }
-};
-
-const getTaskById = (req, res) => {
-  const { id } = req.params;
-  const task = taskData.find((item) => item.id === parseInt(id, 10));
-
-  if (!task) {
-    res.status(200).json({
-      msg: `The task with ID ${id} does not exist`,
-    });
-  } else {
-    res.status(200).json({
-      data: task,
-    });
-  }
-};
-const deleteTask = (req, res) => {
-  const { id } = req.params;
-  const filterTasks = taskData.filter((task) => task.id !== parseInt(id, 10));
-
-  if (taskData.length === filterTasks.length) {
-    res.status(404).json({
-      msg: `${id} not found`,
-    });
-  } else {
-    fs.writeFile('src/data/tasks.json', JSON.stringify(filterTasks), (error) => {
-      if (error) {
-        res.send(error);
-      } else {
-        res.status(200).json({
-          msg: `The tasks ${id} has been deleted`,
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Task could not be created',
+            error,
         });
-      }
-    });
-  }
-};
+    }
+}
 
-const getTasksByHours = (req, res) => {
-  const { hours } = req.params;
-  const filterTasks = taskData.filter((task) => task.hours >= hours);
+// const editTask = (req, res) => {
+//   const { id } = req.params;
+//   const task = taskData.find((item) => item.id === parseInt(id, 10));
 
-  res.status(200).json({
-    data: filterTasks,
-  });
-};
+//   if (!task) {
+//     res.status(200).json({
+//       msg: `The task with ID ${id} does not exist`,
+//     });
+//   } else {
+//     const updTask = req.body;
+//     elements.forEach((prop) => {
+//       task[prop] = updTask[prop] ? updTask[prop] : task[prop];
+//     });
+//     fs.writeFile('src/data/tasks.json', JSON.stringify(taskData), (err) => {
+//       if (err) {
+//         res.send(err);
+//       } else {
+//         res.status(200).json({
+//           msg: 'The task was updated',
+//         });
+//       }
+//     });
+//   }
+// };
 
-export {
-  createTask,
-  editTask,
-  getTaskById,
-  deleteTask,
-  getTasksByHours,
-};
+// const getTaskById = (req, res) => {
+//   const { id } = req.params;
+//   const task = taskData.find((item) => item.id === parseInt(id, 10));
+
+//   if (!task) {
+//     res.status(200).json({
+//       msg: `The task with ID ${id} does not exist`,
+//     });
+//   } else {
+//     res.status(200).json({
+//       data: task,
+//     });
+//   }
+// };
+
+// const deleteTask = (req, res) => {
+//   const { id } = req.params;
+//   const filterTasks = taskData.filter((task) => task.id !== parseInt(id, 10));
+
+//   if (taskData.length === filterTasks.length) {
+//     res.status(404).json({
+//       msg: `${id} not found`,
+//     });
+//   } else {
+//     fs.writeFile('src/data/tasks.json', JSON.stringify(filterTasks), (error) => {
+//       if (error) {
+//         res.send(error);
+//       } else {
+//         res.status(200).json({
+//           msg: `The tasks ${id} has been deleted`,
+//         });
+//       }
+//     });
+//   }
+// };
+
+// const getTasksByHours = (req, res) => {
+//   const { hours } = req.params;
+//   const filterTasks = taskData.filter((task) => task.hours >= hours);
+
+//   res.status(200).json({
+//     data: filterTasks,
+//   });
+// };
+
+export default createTask;
+
+// export {
+//   createTask,
+//   editTask,
+//   getTaskById,
+//   deleteTask,
+//   getTasksByHours,
+// };
