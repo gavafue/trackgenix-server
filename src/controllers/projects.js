674c4886baa718b51bafa1e0f8283fdb47d79857
Johsua Nowak <johsua.nowak@gmail.com>
2022-05-16 20:10:28 -0300

    TG-20: Created method to edit project

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -58,7 +58,45 @@ const deleteProject = async (req, res) => {
   }
 };
 
+const editProject = async (req, res) => {
+  try {
+    if (!req.params) {
+      return res.status(400).json({
+        msg: 'Missing id parameter in request.',
+        data: undefined,
+        error: true,
+      });
+    }
+
+    const result = await ProjectsModels.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true },
+    );
+
+    if (!result) {
+      return res.status(404).json({
+        msg: `The project with id ${req.params.id} can't be found.`,
+        data: undefined,
+        error: true,
+      });
+    }
+    return res.status(200).json({
+      msg: `Project with id ${req.params.id} edited.`,
+      data: undefined,
+      error: false,
+    });
+  } catch (error) {
+    return res.json({
+      msg: `There was an error: ${error}`,
+      data: undefined,
+      error: true,
+    });
+  }
+};
+
 export default {
   createProject,
   deleteProject,
+  editProject,
 };
