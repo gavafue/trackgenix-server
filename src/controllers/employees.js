860ce9e41267e593d4bf2cea167e1268af0c5c74
shirley1702 <shirleyseaton17@gmail.com>
2022-05-17 15:53:33 -0300

    TG-15:I created the file for the validations and gave its functionality

--- src/controllers/employees.js
+++ src/controllers/employees.js
@@ -1,70 +1,80 @@
 import EmployeeSchema from '../models/Employees';
 
-/* isValid = mongoose.Types.ObjectId.isValid('5c0a7922c9d89830f4911426'); */ // true
-
 const getAllEmployee = async (req, res) => {
-  try {
-    const allEmployee = await EmployeeSchema.find(req.query);
-    return res.status(200).json({
-      msg: 'The list has been successfully retrieved',
-      data: allEmployee,
-      error: false,
-    });
-  } catch (error) {
-    return res.status(400).json({
-      msg: 'error',
-      data: undefined,
-      error: true,
-    });
-  }
+    try {
+        let employeeAll = 0;
+        if (req.query) {
+            employeeAll = await EmployeeSchema.find(req.query)
+            if (employeeAll === 0) {
+                return res.status(404).json({
+                    msg: 'no hay nada',
+                    data: undefined,
+                    error: true,
+                });
+            }
+        }
+        else { employeeAll = EmployeeSchema.find({}); }
+        return res.status(200).json({
+            msg: 'The list has been successfully retrieved',
+            data: employeeAll,
+            error: false,
+        });
+    }
+    catch (error) {
+        return res.status(400).json({
+            msg: 'Error',
+            data: undefined,
+            error: true,
+        });
+    }
 };
 
 const getEmployeeById = async (req, res) => {
-  try {
-    if (req.params.id) {
-      const employee = await EmployeeSchema.findById(req.params.id);
-      return res.status(200).json({
-        msg: `This employee with ID ${req.params.id} has been found`,
-        data: employee,
-        error: false,
-      });
+    try {
+        if (req.params.id) {
+            const employee = await EmployeeSchema.findById(req.params.id);
+            return res.status(200).json({
+                msg: `This employee with ID ${req.params.id} has been found`,
+                data: employee,
+                error: false,
+            });
+        }
+        return res.status(404).json({
+            msg: `This time Sheet with ID ${req.params.id} does not exist`,
+            data: undefined,
+            error: true,
+        });
+    } catch (error) {
+        return res.json({
+            msg: 'error',
+            data: undefined,
+            error: true,
+        });
     }
-    return res.status(404).json({
-      msg: `This time Sheet with ID ${req.params.id} does not exist`,
-      data: undefined,
-      error: true,
-    });
-  } catch (error) {
-    return res.json({
-      msg: 'error',
-      data: undefined,
-      error: true,
-    });
-  }
 };
 
 const deleteEmployee = async (req, res) => {
-  try {
-    if (req.params.id) {
-      const employee = await EmployeeSchema.findByIdAndDelete(req.params.id);
-      return res.status(200).json({
-        msg: `This employee with ID ${req.params.id} has been eliminated`,
-        data: employee,
-        error: false,
-      });
+    try {
+        if (req.params.id) {
+            const employee = await EmployeeSchema.findByIdAndDelete(req.params.id);
+            return res.status(200).json({
+                msg: `This employee with ID ${req.params.id} has been eliminated`,
+                data: employee,
+                error: false,
+            });
+        }
+        return res.status(404).json({
+            msg: 'this id no se borro',
+            data: undefined,
+            error: true,
+        });
+    } catch (error) {
+        return res.json({
+            msg: 'error',
+            data: undefined,
+            error: true,
+        });
     }
-    return res.status(404).json({
-      msg: 'this id no se borro',
-      data: undefined,
-      error: true,
-    });
-  } catch (error) {
-    return res.json({
-      msg: 'error',
-      data: undefined,
-      error: true,
-    });
-  }
 };
 
 export default {
--- src/routes/employees.js
+++ src/routes/employees.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import employeesControllers from '../controllers/employees';
+import employeeValidation from '../validations/employees'
 
 const router = express.Router();
 
 router
   .get('/', employeesControllers.getAllEmployee)
-  .get('/:id', employeesControllers.getEmployeeById)
-  .delete('/:id', employeesControllers.deleteEmployee);
+  .get('/:id', employeeValidation.idValidation, employeesControllers.getEmployeeById)
+  .delete('/:id', employeeValidation.idValidation, employeesControllers.deleteEmployee);
 export default router;
