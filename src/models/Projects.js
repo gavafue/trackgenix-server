82f8c4bfc7104aa00b16fbf51aee1bdb43bbce8e
Johsua Nowak <johsua.nowak@gmail.com>
2022-05-17 11:47:36 -0300

    TG-20: Fixed curly brace in schema

--- src/models/Projects.js
+++ src/models/Projects.js
@@ -13,7 +13,7 @@ const projectSchema = new Schema(
     ],
     name: { type: String, required: true },
     startDate: { type: Date, required: true },
-    endDate: { type: Date},
+    endDate: { type: Date },
     description: { type: String, required: true },
     active: { type: Boolean, required: true },
     client: { type: String, required: true },
