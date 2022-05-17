24e8282b2f1c501dc249d870bceaa7a9629f756d
shirley1702 <shirleyseaton17@gmail.com>
2022-05-17 12:31:59 -0300

    TG-15:I made the corresponding routes for the employee method

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import * as tasksController from './controllers/tasks';
-import * as timeSheetControllers from './controllers/time-sheets';
-import * as projectsControllers from './controllers/projects';
-import * as employeesControllers from './controllers/employees';
-import * as superadminControllers from './controllers/super-admins';
-import * as adminsControllers from './controllers/admins';
+import router from './routes';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,55 +8,7 @@ const port = process.env.PORT || 3000;
 const MONGO_URL = 'mongodb+srv://radiumRocket:8dll6U2hMKSETFAK@trackgenix.0a7hs.mongodb.net/Trackgenix?retryWrites=true&w=majority';
 
 app.use(express.json());
-
-// Routes tasks
-app.post('/tasks/add', tasksController.createTask);
-app.put('/tasks/edit/:id', tasksController.editTask);
-app.get('/tasks/:id', tasksController.getTaskById);
-app.delete('/delete/task/:id', tasksController.deleteTask);
-app.get('/tasks/hours/:hours', tasksController.getTasksByHours);
-
-// Routes employees
-app.get('/employees', employeesControllers.getAllEmployee);
-app.get('/employees/:id', employeesControllers.getOnlyId);
-app.get('/employees/country/:country', employeesControllers.filterByCountry);
-app.post('/employees', employeesControllers.createMember);
-app.put('/employee/:id', employeesControllers.editEmployeeById);
-app.delete('/employee/:id', employeesControllers.deleteEmployeeById);
-app.get('/employee/:lastName', employeesControllers.filterByLastName);
-
-// Routes time sheet
-app.post('/timeSheet', timeSheetControllers.createTimeSheet);
-app.delete('/timeSheet/:id', timeSheetControllers.deleteTimeSheet);
-app.get('/timeSheet/:employeeName', timeSheetControllers.filterTSByName);
-app.get('/timeSheet/:id', timeSheetControllers.gettimeSheetById);
-app.put('/timeSheet/:id', timeSheetControllers.editTimeSheet);
-app.get('/timeSheetByProject/:project', timeSheetControllers.filterTSheetProject);
-
-// Routes admins
-app.get('/admins', adminsControllers.getAllAdmins);
-app.post('/admins/add', adminsControllers.addAdmin);
-app.put('/admins/update/:id', adminsControllers.updateAdmin);
-app.get('/admins/getById/:id', adminsControllers.getAdminById);
-app.delete('/admins/deleteById/:id', adminsControllers.deleteAdminById);
-app.get('/admins/getByGender', adminsControllers.getAdminByGender);
-app.get('/admins/getByName', adminsControllers.getAdminByName);
-
-// Routes projects
-app.post('/project/add', projectsControllers.createProject);
-app.put('/project/edit/:id', projectsControllers.editProject);
-app.get('/project/:id', projectsControllers.getProjectById);
-app.delete('/delete/project/:id', projectsControllers.deleteProjects);
-app.get('/projects/', projectsControllers.getProjects);
-app.post('/:projectId/addMember', projectsControllers.assignRP);
-
-// Routes superadmins
-app.get('/superadmins', superadminControllers.getAllSa);
-app.get('/superadmins/getById/:id', superadminControllers.getSaById);
-app.post('/superadmins/add', superadminControllers.createSa);
-app.delete('/superadmins/delete/:id', superadminControllers.deleteSa);
-app.get('/superadmins/getActive', superadminControllers.getActiveSa);
-app.put('/superadmins/edit/:id', superadminControllers.editSa);
+app.use(router);
 
 mongoose.connect(
   MONGO_URL,
---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+import express from "express";
+import employeesRoutes from './employees';
+
+const router = express.Router();
+
+router
+    .use('/employees',employeesRoutes);
+export default router
