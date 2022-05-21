0178d4a736ce0f18a022d6d2a30dd0c0b2840003
iaracriscenti <iaracriscenti@gmail.com>
2022-05-21 17:33:42 -0300

    TG-35: Added admin seed file and some tests to check if it works

--- src/tests/admins.test.js
+++ src/tests/admins.test.js
@@ -1,1 +1,20 @@
-test.todo('add required tests');
+import request from 'supertest';
+import app from '../app';
+import Admins from '../models/Admins';
+import adminsSeed from '../seeds/admins';
+
+beforeAll(async () => {
+  await Admins.collection.insertMany(adminsSeed);
+});
+
+describe('GET /admins', () => {
+  test('response should return a 200 status', async () => {
+    const response = await request(app).get('/admins').send();
+    expect(response.status).toBe(200);
+  });
+
+  test('response should return false error', async () => {
+    const response = await request(app).get('/admins').send();
+    expect(response.error).toBe(false);
+  });
+});
