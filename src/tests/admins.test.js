09f89efa846aa295ac1ec49570cc73d7cd72393a
iaracriscenti <iaracriscenti@gmail.com>
2022-05-22 15:32:17 -0300

    TG-35: Added more describe functions to be more specific about the tests

--- src/tests/admins.test.js
+++ src/tests/admins.test.js
@@ -12,64 +12,60 @@ let adminID;
 let randomID;
 
 describe('GET /admins', () => {
-  test('response should return a 200 status', async () => {
-    const response = await request(app).get('/admins').send();
-    expect(response.status).toBe(200);
+  describe('when the user wants the full list of admins', () => {
+    test('response should return a 200 status', async () => {
+      const response = await request(app).get('/admins').send();
+      expect(response.status).toBe(200);
+    });
+    test('response should return false error', async () => {
+      const response = await request(app).get('/admins').send();
+      expect(response.body.error).toBeFalsy();
+    });
+    test('response should return a successful message', async () => {
+      const response = await request(app).get('/admins').send();
+      expect(response.body.message).toEqual('The request was successful');
+    });
+    test('response should return at least one admin', async () => {
+      const response = await request(app).get('/admins').send();
+      expect(response.body.data.length).toBeGreaterThan(0);
+    });
   });
-
-  test('response should return a 200 status', async () => {
-    const response = await request(app).get('/admins?active=true').send();
-    expect(response.status).toBe(200);
+  describe('when the user wants a filtered list of admins and it is not empty', () => {
+    test('response should return a 200 status', async () => {
+      const response = await request(app).get('/admins?active=true').send();
+      expect(response.status).toBe(200);
+    });
+    test('response should return false error', async () => {
+      const response = await request(app).get('/admins').send();
+      expect(response.body.error).toBeFalsy();
+    });
+    test('response should return a successful message', async () => {
+      const response = await request(app).get('/admins?active=true').send();
+      expect(response.body.message).toEqual('The request was successful');
+    });
+    test('response should return at least one admin', async () => {
+      const response = await request(app).get('/admins?active=true').send();
+      expect(response.body.data.length).toBeGreaterThan(0);
+    });
   });
+  describe('when the user wants a filtered list of admins and it is empty', () => {
+    test('response should return a 404 status', async () => {
+      const response = await request(app).get('/admins?name=').send();
+      expect(response.status).toBe(404);
+    });
+    test('response should return true error', async () => {
+      const response = await request(app).get('/admins?name=').send();
+      expect(response.body.error).toBeTruthy();
+    });
 
-  test('response should return a 404 status', async () => {
-    const response = await request(app).get('/admins?name=').send();
-    expect(response.status).toBe(404);
-  });
-
-  test('response should return false error', async () => {
-    const response = await request(app).get('/admins').send();
-    expect(response.body.error).toBeFalsy();
-  });
-
-  test('response should return false error', async () => {
-    const response = await request(app).get('/admins?active=true').send();
-    expect(response.body.error).toBeFalsy();
-  });
-
-  test('response should return true error', async () => {
-    const response = await request(app).get('/admins?name=').send();
-    expect(response.body.error).toBeTruthy();
-  });
-
-  test('response should return a successful message', async () => {
-    const response = await request(app).get('/admins').send();
-    expect(response.body.message).toEqual('The request was successful');
-  });
-
-  test('response should return a successful message', async () => {
-    const response = await request(app).get('/admins?active=true').send();
-    expect(response.body.message).toEqual('The request was successful');
-  });
-
-  test('response should return a not found message', async () => {
-    const response = await request(app).get('/admins?name=').send();
-    expect(response.body.message).toEqual('Admins not found');
-  });
-
-  test('response should return at least one admin', async () => {
-    const response = await request(app).get('/admins').send();
-    expect(response.body.data.length).toBeGreaterThan(0);
-  });
-
-  test('response should return at least one admin', async () => {
-    const response = await request(app).get('/admins?active=true').send();
-    expect(response.body.data.length).toBeGreaterThan(0);
-  });
-
-  test('response should not return an admin', async () => {
-    const response = await request(app).get('/admins?name=').send();
-    expect(response.body.data).toBeUndefined();
+    test('response should return a not found message', async () => {
+      const response = await request(app).get('/admins?name=').send();
+      expect(response.body.message).toEqual('Admins not found');
+    });
+    test('response should not return an admin', async () => {
+      const response = await request(app).get('/admins?name=').send();
+      expect(response.body.data).toBeUndefined();
+    });
   });
 });
 
@@ -86,50 +82,47 @@ describe('POST /admins', () => {
     active: false,
     dateBirth: '08/06/1978',
   };
-
-  test('response should return a 201 status', async () => {
-    const response = await request(app).post('/admins').send(newAdmin);
-    expect(response.status).toBe(201);
-    // eslint-disable-next-line no-underscore-dangle
-    adminID = response.body.data._id;
+  describe('when the admin entered is created', () => {
+    test('response should return a 201 status', async () => {
+      const response = await request(app).post('/admins').send(newAdmin);
+      expect(response.status).toBe(201);
+      // eslint-disable-next-line no-underscore-dangle
+      adminID = response.body.data._id;
+    });
+    test('response should return a false error', async () => {
+      newAdmin.email = 'newEmail@gmail.com';
+      const response = await request(app).post('/admins').send(newAdmin);
+      expect(response.body.error).toBeFalsy();
+    });
+    test('response should return success message', async () => {
+      newAdmin.email = 'newEmail-2@gmail.com';
+      const response = await request(app).post('/admins').send(newAdmin);
+      expect(response.body.message).toEqual('Admin created successfully');
+    });
+    test('response should return the created admin', async () => {
+      newAdmin.email = 'newEmail-3@gmail.com';
+      const response = await request(app).post('/admins').send(newAdmin);
+      expect(response.body.data).toBeDefined();
+    });
   });
 
-  test('response should return a 400 status', async () => {
-    const response = await request(app).post('/admins').send(newAdmin);
-    expect(response.status).toBe(400);
-  });
-
-  test('response should return a true error', async () => {
-    const response = await request(app).post('/admins').send(newAdmin);
-    expect(response.body.error).toBeTruthy();
-  });
-
-  test('response should return a false error', async () => {
-    newAdmin.email = 'newEmail@gmail.com';
-    const response = await request(app).post('/admins').send(newAdmin);
-    expect(response.body.error).toBeFalsy();
-  });
-
-  test('response should return an already exist message', async () => {
-    const response = await request(app).post('/admins').send(newAdmin);
-    expect(response.body.message).toEqual('An admin with this email already exists');
-  });
-
-  test('response should return success message', async () => {
-    newAdmin.email = 'newEmail-2@gmail.com';
-    const response = await request(app).post('/admins').send(newAdmin);
-    expect(response.body.message).toEqual('Admin created successfully');
-  });
-
-  test('response should return an undefined data', async () => {
-    const response = await request(app).post('/admins').send(newAdmin);
-    expect(response.body.data).toBeUndefined();
-  });
-
-  test('response should return the created admin', async () => {
-    newAdmin.email = 'newEmail-3@gmail.com';
-    const response = await request(app).post('/admins').send(newAdmin);
-    expect(response.body.data).toBeDefined();
+  describe('when the admin entered has an email already used', () => {
+    test('response should return a 400 status', async () => {
+      const response = await request(app).post('/admins').send(newAdmin);
+      expect(response.status).toBe(400);
+    });
+    test('response should return a true error', async () => {
+      const response = await request(app).post('/admins').send(newAdmin);
+      expect(response.body.error).toBeTruthy();
+    });
+    test('response should return an already exist message', async () => {
+      const response = await request(app).post('/admins').send(newAdmin);
+      expect(response.body.message).toEqual('An admin with this email already exists');
+    });
+    test('response should return an undefined data', async () => {
+      const response = await request(app).post('/admins').send(newAdmin);
+      expect(response.body.data).toBeUndefined();
+    });
   });
 });
 
