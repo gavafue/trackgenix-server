6078d83197e5db4e6dffba5f68805e102369b856
AlejandroSantini <alejandro.j.santini@gmail.com>
2022-05-22 19:25:26 -0300

    TG-39: Test succes for get all

---
+++ src/seeds/tasks.js
@@ -0,0 +1,44 @@
+import mongoose from 'mongoose';
+
+export default [
+  {
+    id: mongoose.Types.ObjectId('628ab5b2fc13ae2a48000064'),
+    nameProject: 'New project',
+    week: 1,
+    day: 8,
+    description: 'This is a new project for tests',
+    hours: 5,
+  },
+  {
+    id: mongoose.Types.ObjectId('628ab5b2fc13ae2a48000065'),
+    nameProject: 'Other new project',
+    week: 3,
+    day: 9,
+    description: 'This is a new project for tests',
+    hours: 9,
+  },
+  {
+    id: mongoose.Types.ObjectId('628ab5b2fc13ae2a48000066'),
+    nameProject: 'Ooooother new project',
+    week: 2,
+    day: 7,
+    description: 'This is a new project for tests',
+    hours: 8,
+  },
+  {
+    id: mongoose.Types.ObjectId('628ab5b2fc13ae2a48000067'),
+    nameProject: 'One more',
+    week: 4,
+    day: 6,
+    description: 'This is a new project for tests',
+    hours: 6,
+  },
+  {
+    id: mongoose.Types.ObjectId('628ab5b2fc13ae2a48000068'),
+    nameProject: 'Ooother project',
+    week: 2,
+    day: 4,
+    description: 'This is a new project for tests',
+    hours: 3,
+  },
+];
--- src/tests/tasks.test.js
+++ src/tests/tasks.test.js
@@ -1,1 +1,20 @@
-test.todo('add required tests');
+import request from 'supertest';
+import app from '../app';
+import Tasks from '../models/Tasks';
+import tasksSeed from '../seeds/tasks';
+
+beforeAll(async () => {
+  await Tasks.collection.insertMany(tasksSeed);
+});
+
+describe('GET ALL /tasks', () => {
+  test('response should return a 200 status', async () => {
+    const response = await request(app).get('/tasks').send();
+    expect(response.status).toBe(200);
+  });
+
+  test('response should return false error', async () => {
+    const response = await request(app).get('/tasks').send();
+    expect(response.error).toBe(false);
+  });
+});
