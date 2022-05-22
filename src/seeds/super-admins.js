37647b3a1c35c116a599d72857905b9db3819da1
Johsua Nowak <johsua.nowak@gmail.com>
2022-05-22 12:57:39 -0300

    TG-36: Created some seeds and made basic tests on GET method

---
+++ src/seeds/super-admins.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+export default [{
+  _id: mongoose.Types.ObjectId('628947336a58a75cb236a211'),
+  first_name: 'Joseph',
+  last_name: 'Joestar',
+  password: '1234abcd',
+  role: 'SA',
+  email: 'jo.jo@swfoundation.com',
+  active: true,
+},
+{
+  _id: mongoose.Types.ObjectId('62894865e637467db9f844cc'),
+  first_name: 'Giorno',
+  last_name: 'Giovanna',
+  password: '1t4r14',
+  role: 'SA',
+  email: 'golden.w@ventoaureo.com',
+  active: true,
+},
+{
+  _id: mongoose.Types.ObjectId('628a527fcf385c8f4627df3c'),
+  first_name: 'Dio',
+  last_name: 'Brando',
+  password: 'Zaw4rud0',
+  role: 'SA',
+  email: 'dio.muda@konodda.com',
+  active: false,
+}];
--- src/tests/super-admins.test.js
+++ src/tests/super-admins.test.js
@@ -1,1 +1,25 @@
-test.todo('add required tests');
+import request from 'supertest';
+import app from '../app';
+import SuperAdmins from '../models/Super-admins';
+import superAdminsSeed from '../seeds/super-admins';
+
+beforeAll(async () => {
+  await SuperAdmins.collection.insertMany(superAdminsSeed);
+});
+
+describe('GET /super-admins', () => {
+  test('Response should return a 200 status', async () => {
+    const response = await request(app).get('/super-admin').send();
+    expect(response.status).toBe(200);
+  });
+
+  test('Response should return error false', async () => {
+    const response = await request(app).get('/super-admin').send();
+    expect(response.error).toBe(false);
+  });
+
+  test('Response should return at least one super admin', async () => {
+    const response = await request(app).get('/super-admin').send();
+    expect(response.body.data.length).toBeGreaterThan(0);
+  });
+});
