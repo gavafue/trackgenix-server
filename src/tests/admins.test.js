008a51c156eba6acbcfd94e78ce698394d2b3396
iaracriscenti <iaracriscenti@gmail.com>
2022-05-22 10:01:22 -0300

    TG-35: Added tests for endpoint GET /admins

--- src/tests/admins.test.js
+++ src/tests/admins.test.js
@@ -13,8 +13,58 @@ describe('GET /admins', () => {
     expect(response.status).toBe(200);
   });
 
+  test('response should return a 200 status', async () => {
+    const response = await request(app).get('/admins?active=true').send();
+    expect(response.status).toBe(200);
+  });
+
+  test('response should return a 404 status', async () => {
+    const response = await request(app).get('/admins?name=').send();
+    expect(response.status).toBe(404);
+  });
+
   test('response should return false error', async () => {
     const response = await request(app).get('/admins').send();
-    expect(response.error).toBe(false);
+    expect(response.body.error).toBeFalsy();
+  });
+
+  test('response should return false error', async () => {
+    const response = await request(app).get('/admins?active=true').send();
+    expect(response.body.error).toBeFalsy();
+  });
+
+  test('response should return true error', async () => {
+    const response = await request(app).get('/admins?name=').send();
+    expect(response.body.error).toBeTruthy();
+  });
+
+  test('response should return a successful message', async () => {
+    const response = await request(app).get('/admins').send();
+    expect(response.body.message).toEqual('The request was successful');
+  });
+
+  test('response should return a successful message', async () => {
+    const response = await request(app).get('/admins?active=true').send();
+    expect(response.body.message).toEqual('The request was successful');
+  });
+
+  test('response should return a not found message', async () => {
+    const response = await request(app).get('/admins?name=').send();
+    expect(response.body.message).toEqual('Admins not found');
+  });
+
+  test('response should return at least one admin', async () => {
+    const response = await request(app).get('/admins').send();
+    expect(response.body.data.length).toBeGreaterThan(0);
+  });
+
+  test('response should return at least one admin', async () => {
+    const response = await request(app).get('/admins?active=true').send();
+    expect(response.body.data.length).toBeGreaterThan(0);
+  });
+
+  test('response should not return an admin', async () => {
+    const response = await request(app).get('/admins?name=').send();
+    expect(response.body.data).toBeUndefined();
   });
 });
