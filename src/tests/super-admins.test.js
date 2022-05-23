550f559fdc8887839195fad2a35773eb58b099be
Johsua Nowak <johsua.nowak@gmail.com>
2022-05-23 20:09:50 -0300

    TG-36: Created extra tests for controllers in super admin

--- src/tests/super-admins.test.js
+++ src/tests/super-admins.test.js
@@ -23,6 +23,31 @@ describe('GET /super-admins', () => {
     const response = await request(app).get('/super-admin').send();
     expect(response.body.data.length).toBeGreaterThan(0);
   });
+
+  test('Response message should be the same as in controller', async () => {
+    const response = await request(app).get('/super-admin').send();
+    expect(response.body.message).toBe('The request was made successfully');
+  });
+
+  test('Response should return a 404 status', async () => {
+    const response = await request(app).get('/super-admin?firstName=Diavolo').send();
+    expect(response.status).toBe(404);
+  });
+
+  test('Response message should be the same as in controller', async () => {
+    const response = await request(app).get('/super-admin?firstName=Diavolo').send();
+    expect(response.body.message).toBe('Super admin was not found');
+  });
+
+  test('Response should return undefined data', async () => {
+    const response = await request(app).get('/super-admin?firstName=Diavolo').send();
+    expect(response.body.data).toBe(undefined);
+  });
+
+  test('Response should return error true', async () => {
+    const response = await request(app).get('/super-admin?firstName=Diavolo').send();
+    expect(response.body.error).toBe(true);
+  });
 });
 
 let superAdminId1;
@@ -67,6 +92,154 @@ describe('POST /super-admins', () => {
     expect(response.body.data).not.toBe(undefined);
     superAdminId3 = response.body.data._id;
   });
+
+  test('Response should return error true', async () => {
+    const response = await request(app).post('/super-admin').send({
+      firstName: 'Risotto',
+      lastName: 'Nero',
+      password: 'M3t4ll1c4',
+      role: 'QA',
+      email: 'risotto.nero@squadra.com',
+      active: false,
+    });
+    expect(response.body.error).toBe(true);
+  });
+
+  test('Response should return a 200 status', async () => {
+    const response = await request(app).post('/super-admin').send({
+      firstName: 'Risotto',
+      lastName: 'Nero',
+      password: 'M3t4ll1c4',
+      role: 'SA',
+      email: 'risotto.n@squadra.com',
+      active: false,
+    });
+    expect(response.status).toBe(200);
+  });
+
+  test('Response should return error true', async () => {
+    const response = await request(app).post('/super-admin').send({
+      firstName: 'Risotto',
+      lastName: 'Nero',
+      password: 'M3t4ll1c4',
+      role: 'SA',
+      email: 'risotto.n@squadra.com',
+      active: false,
+    });
+    expect(response.body.error).toBe(true);
+  });
+
+  test('Response should return undefined data', async () => {
+    const response = await request(app).post('/super-admin').send({
+      firstName: 'Risotto',
+      lastName: 'Nero',
+      password: 'M3t4ll1c4',
+      role: 'SA',
+      email: 'risotto.n@squadra.com',
+      active: false,
+    });
+    expect(response.body.data).toBe(undefined);
+  });
+
+  test('Response message should be the same as in controller', async () => {
+    const response = await request(app).post('/super-admin').send({
+      firstName: 'Risotto',
+      lastName: 'Nero',
+      password: 'M3t4ll1c4',
+      role: 'SA',
+      email: 'risotto.n@squadra.com',
+      active: false,
+    });
+    expect(response.body.message).toBe('Super admin with this email already exists');
+  });
+
+  test('Response should return error true', async () => {
+    const response = await request(app).post('/super-admin').send({
+      lastName: 'Nero',
+      password: 'M3t4ll1c4',
+      role: 'SA',
+      email: 'risotto.nero@squadra.com',
+      active: false,
+    });
+    expect(response.body.error).toBe(true);
+  });
+
+  test('Response should return a 400 status', async () => {
+    const response = await request(app).post('/super-admin').send({
+      lastName: 'Nero',
+      password: 'M3t4ll1c4',
+      role: 'SA',
+      email: 'risotto.nero@squadra.com',
+      active: false,
+    });
+    expect(response.status).toBe(400);
+  });
+
+  test('Response should return undefined data', async () => {
+    const response = await request(app).post('/super-admin').send({
+      lastName: 'Nero',
+      password: 'M3t4ll1c4',
+      role: 'SA',
+      email: 'risotto.nero@squadra.com',
+      active: false,
+    });
+    expect(response.body.data).toBe(undefined);
+  });
+
+  test('Response should return error true', async () => {
+    const response = await request(app).post('/super-admin').send({
+      firstName: 'Risotto',
+      password: 'M3t4ll1c4',
+      role: 'SA',
+      email: 'risotto.n@squadra.com',
+      active: false,
+    });
+    expect(response.body.error).toBe(true);
+  });
+
+  test('Response should return error true', async () => {
+    const response = await request(app).post('/super-admin').send({
+      firstName: 'Risotto',
+      lastName: 'Nero',
+      role: 'SA',
+      email: 'risotto.n@squadra.com',
+      active: false,
+    });
+    expect(response.body.error).toBe(true);
+  });
+
+  test('Response should return error true', async () => {
+    const response = await request(app).post('/super-admin').send({
+      firstName: 'Risotto',
+      lastName: 'Nero',
+      password: 'M3t4ll1c4',
+      email: 'risotto.n@squadra.com',
+      active: false,
+    });
+    expect(response.body.error).toBe(true);
+  });
+
+  test('Response should return error true', async () => {
+    const response = await request(app).post('/super-admin').send({
+      firstName: 'Risotto',
+      lastName: 'Nero',
+      password: 'M3t4ll1c4',
+      role: 'SA',
+      active: false,
+    });
+    expect(response.body.error).toBe(true);
+  });
+
+  test('Response should return error true', async () => {
+    const response = await request(app).post('/super-admin').send({
+      firstName: 'Risotto',
+      lastName: 'Nero',
+      password: 'M3t4ll1c4',
+      role: 'SA',
+      email: 'risotto.n@squadra.com',
+    });
+    expect(response.body.error).toBe(true);
+  });
 });
 
 describe('GET /super-admins/:id', () => {
@@ -84,17 +257,35 @@ describe('GET /super-admins/:id', () => {
     const response = await request(app).get(`/super-admin/${superAdminId1}`).send();
     expect(response.body.data).not.toBe(undefined);
   });
-});
 
-describe('DELETE /super-admins/:id', () => {
-  test('Response should return a 200 status', async () => {
-    const response = await request(app).delete(`/super-admin/${superAdminId1}`).send();
-    expect(response.status).toBe(200);
+  test('Response message should be the same as in controller', async () => {
+    const response = await request(app).get(`/super-admin/${superAdminId1}`).send();
+    expect(response.body.message).toBe('The request was successful');
   });
 
-  test('Response should return error false', async () => {
-    const response = await request(app).delete(`/super-admin/${superAdminId2}`).send();
-    expect(response.error).toBe(false);
+  test('Response should return correct super admin', async () => {
+    const response = await request(app).get(`/super-admin/${superAdminId1}`).send();
+    expect(response.body.data.email).toBe('jo.jo@swmarine.com');
+  });
+
+  test('Response should return a 404 status', async () => {
+    const response = await request(app).get('/super-admin/628c086f48e6f9779ae82eac').send();
+    expect(response.status).toBe(404);
+  });
+
+  test('Response should return error true', async () => {
+    const response = await request(app).get('/super-admin/628c086f48e6f9779ae82eac').send();
+    expect(response.body.error).toBe(true);
+  });
+
+  test('Response should return undefined data', async () => {
+    const response = await request(app).get('/super-admin/628c086f48e6f9779ae82eac').send();
+    expect(response.body.data).toBe(undefined);
+  });
+
+  test('Response message should be the same as in controller', async () => {
+    const response = await request(app).get('/super-admin/628c086f48e6f9779ae82eac').send();
+    expect(response.body.message).toBe('The superadmin with id 628c086f48e6f9779ae82eac has not been found');
   });
 });
 
@@ -121,5 +312,47 @@ describe('PUT /super-admins/:id', () => {
       email: 'pesci@squadra.com',
     });
     expect(response.body.data).not.toBe(undefined);
+  });
+});
+
+describe('DELETE /super-admins/:id', () => {
+  test('Response should return a 200 status(success)', async () => {
+    const response = await request(app).delete(`/super-admin/${superAdminId1}`).send();
+    expect(response.status).toBe(200);
+  });
+
+  test('Response should return error false(success)', async () => {
+    const response = await request(app).delete(`/super-admin/${superAdminId2}`).send();
+    expect(response.error).toBe(false);
+  });
+
+  test('Response message should be the same as in controller(success)', async () => {
+    const response = await request(app).delete(`/super-admin/${superAdminId3}`).send();
+    expect(response.body.message).toBe('The Super admin has been susccessfully deleted');
+  });
+
+  test('Response should return a 404 status(no id sent)', async () => {
+    const response = await request(app).delete('/super-admin/').send();
+    expect(response.status).toBe(404);
+  });
+
+  test('Response should return a 404 status(id not in database)', async () => {
+    const response = await request(app).delete('/super-admin/628bcfc73699c2ac94a02a8c').send();
+    expect(response.status).toBe(404);
+  });
+
+  test('Response message should be the same as in controller(id not in database)', async () => {
+    const response = await request(app).delete('/super-admin/628bcfc73699c2ac94a02a8c').send();
+    expect(response.body.message).toBe('The Super admin has not been found');
+  });
+
+  test('Response should return undefined data(id not in database)', async () => {
+    const response = await request(app).delete('/super-admin/628bcfc73699c2ac94a02a8c').send();
+    expect(response.body.data).toBe(undefined);
+  });
+
+  test('Response should return error true', async () => {
+    const response = await request(app).delete('/super-admin/628bcfc73699c2ac94a02a8c').send();
+    expect(response.body.error).toBe(true);
   });
 });
