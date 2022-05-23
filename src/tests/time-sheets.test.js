82fd193006510280325ce6a8334721ea657029c2
Marco Migliaro <marcomig96@gmail.com>
2022-05-23 14:52:07 -0300

    TG-38: Added tests for POST in time sheet

--- src/tests/time-sheets.test.js
+++ src/tests/time-sheets.test.js
@@ -1,5 +1,5 @@
 import request from 'supertest';
-// import mongoose from 'mongoose';
+import mongoose from 'mongoose';
 import app from '../app';
 import TimeSheets from '../models/Time-sheets';
 import Projects from '../models/Projects';
@@ -36,56 +36,56 @@ describe('GET /timeSheets', () => {
   });
 });
 
-// describe('POST /timeSheets', () => {
-//   test('response should return a 201 status', async () => {
-//     const response = await request(app).post('/timeSheets').send({
-//       project: mongoose.Types.ObjectId('628bb53a8667fb09f64dd9fd'),
-//       employee: mongoose.Types.ObjectId('60d4a32f257e066e8495ce12'),
-//       weekSprint: 7,
-//       date: '2021-10-21T03:00:00.000Z',
-//       hoursWorked: 5,
-//       hoursProject: 15,
-//       workDescription: 'Created server',
-//     });
-//     expect(response.status).toBe(201);
-//   });
+describe('POST /timeSheets', () => {
+  test('response should return a 201 status', async () => {
+    const response = await request(app).post('/timeSheets').send({
+      project: mongoose.Types.ObjectId('628bb53a8667fb09f64dd9fd'),
+      employee: mongoose.Types.ObjectId('60d4a32f257e066e8495ce12'),
+      weekSprint: 7,
+      date: '2021-10-21T03:00:00.000Z',
+      hoursWorked: 5,
+      hoursProject: 15,
+      workDescription: 'Created server',
+    });
+    expect(response.status).toBe(201);
+  });
 
-//   test('response should return a successful message', async () => {
-//     const response = await request(app).post('/timeSheets').send({
-//       project: mongoose.Types.ObjectId('628bb53a8667fb09f64dd9fd'),
-//       employee: mongoose.Types.ObjectId('60d4a32f257e066e8495ce12'),
-//       weekSprint: 7,
-//       date: '2021-10-21T03:00:00.000Z',
-//       hoursWorked: 5,
-//       hoursProject: 15,
-//       workDescription: 'Created server',
-//     });
-//     expect(response.body.message).toBe('Time-Sheet created successfully');
-//   });
+  test('response should return a successful message', async () => {
+    const response = await request(app).post('/timeSheets').send({
+      project: mongoose.Types.ObjectId('628bb53a8667fb09f64dd9fd'),
+      employee: mongoose.Types.ObjectId('60d4a32f257e066e8495ce12'),
+      weekSprint: 7,
+      date: '2021-10-21T03:00:00.000Z',
+      hoursWorked: 5,
+      hoursProject: 15,
+      workDescription: 'Created server',
+    });
+    expect(response.body.message).toBe('Time-Sheet created successfully');
+  });
 
-//   test('response should return the created time sheet', async () => {
-//     const response = await request(app).post('/timeSheets').send({
-//       project: mongoose.Types.ObjectId('628bb53a8667fb09f64dd9fd'),
-//       employee: mongoose.Types.ObjectId('60d4a32f257e066e8495ce12'),
-//       weekSprint: 7,
-//       date: '2021-10-21T03:00:00.000Z',
-//       hoursWorked: 5,
-//       hoursProject: 15,
-//       workDescription: 'Created server',
-//     });
-//     expect(response.body.data).toBeDefined();
-//   });
+  test('response should return the created time sheet', async () => {
+    const response = await request(app).post('/timeSheets').send({
+      project: mongoose.Types.ObjectId('628bb53a8667fb09f64dd9fd'),
+      employee: mongoose.Types.ObjectId('60d4a32f257e066e8495ce12'),
+      weekSprint: 7,
+      date: '2021-10-21T03:00:00.000Z',
+      hoursWorked: 5,
+      hoursProject: 15,
+      workDescription: 'Created server',
+    });
+    expect(response.body.data).toBeDefined();
+  });
 
-//   test('response should return false error', async () => {
-//     const response = await request(app).post('/timeSheets').send({
-//       project: mongoose.Types.ObjectId('628bb53a8667fb09f64dd9fd'),
-//       employee: mongoose.Types.ObjectId('60d4a32f257e066e8495ce12'),
-//       weekSprint: 7,
-//       date: '2021-10-21T03:00:00.000Z',
-//       hoursWorked: 5,
-//       hoursProject: 15,
-//       workDescription: 'Created server',
-//     });
-//     expect(response.error).toBe(false);
-//   });
-// });
+  test('response should return false error', async () => {
+    const response = await request(app).post('/timeSheets').send({
+      project: mongoose.Types.ObjectId('628bb53a8667fb09f64dd9fd'),
+      employee: mongoose.Types.ObjectId('60d4a32f257e066e8495ce12'),
+      weekSprint: 7,
+      date: '2021-10-21T03:00:00.000Z',
+      hoursWorked: 5,
+      hoursProject: 15,
+      workDescription: 'Created server',
+    });
+    expect(response.error).toBe(false);
+  });
+});
