dc271981ac0bfadc8b7b81191f6fdcad5ca0fbac
Johsua Nowak <johsua.nowak@gmail.com>
2022-05-22 21:26:28 -0300

    TG-36: Fixed seeds format and crated basic tests for GET, DELETE, POST and PUT

--- src/seeds/super-admins.js
+++ src/seeds/super-admins.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 export default [{
   _id: mongoose.Types.ObjectId('628947336a58a75cb236a211'),
-  first_name: 'Joseph',
-  last_name: 'Joestar',
+  firstName: 'Joseph',
+  lastName: 'Joestar',
   password: '1234abcd',
   role: 'SA',
   email: 'jo.jo@swfoundation.com',
@@ -11,8 +11,8 @@ export default [{
 },
 {
   _id: mongoose.Types.ObjectId('62894865e637467db9f844cc'),
-  first_name: 'Giorno',
-  last_name: 'Giovanna',
+  firstName: 'Giorno',
+  lastName: 'Giovanna',
   password: '1t4r14',
   role: 'SA',
   email: 'golden.w@ventoaureo.com',
@@ -20,10 +20,28 @@ export default [{
 },
 {
   _id: mongoose.Types.ObjectId('628a527fcf385c8f4627df3c'),
-  first_name: 'Dio',
-  last_name: 'Brando',
+  firstName: 'Dio',
+  lastName: 'Brando',
   password: 'Zaw4rud0',
   role: 'SA',
   email: 'dio.muda@konodda.com',
   active: false,
+},
+{
+  _id: mongoose.Types.ObjectId('628a8fffd8b5e6f80dc11868'),
+  firstName: 'Bruno',
+  lastName: 'Bucciarati',
+  password: 'Z1pp3rm4n',
+  role: 'SA',
+  email: 'bucciarati.bruno@passione.com',
+  active: false,
+},
+{
+  _id: mongoose.Types.ObjectId('628a97f07102b9d99e4dc718'),
+  firstName: 'Okuyasu',
+  lastName: 'Nijimura',
+  password: 'Tr1ll1on',
+  role: 'SA',
+  email: 'oku.nij@hando.com',
+  active: true,
 }];
--- src/tests/super-admins.test.js
+++ src/tests/super-admins.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-underscore-dangle */
 import request from 'supertest';
 import app from '../app';
 import SuperAdmins from '../models/Super-admins';
@@ -21,5 +22,104 @@ describe('GET /super-admins', () => {
   test('Response should return at least one super admin', async () => {
     const response = await request(app).get('/super-admin').send();
     expect(response.body.data.length).toBeGreaterThan(0);
+  });
+});
+
+let superAdminId1;
+let superAdminId2;
+let superAdminId3;
+describe('POST /super-admins', () => {
+  test('Response should return a 201 status', async () => {
+    const response = await request(app).post('/super-admin').send({
+      firstName: 'Jotaro',
+      lastName: 'Kujo',
+      password: 'Y4r3Y4r3D4z3',
+      role: 'SA',
+      email: 'jo.jo@swmarine.com',
+      active: true,
+    });
+    expect(response.status).toBe(201);
+    superAdminId1 = response.body.data._id;
+  });
+
+  test('Response should return error false', async () => {
+    const response = await request(app).post('/super-admin').send({
+      firstName: 'Jotaro',
+      lastName: 'Kujo',
+      password: 'Y4r3Y4r3D4z3',
+      role: 'SA',
+      email: 'jo.j0@swmarine.com',
+      active: true,
+    });
+    expect(response.error).toBe(false);
+    superAdminId2 = response.body.data._id;
+  });
+
+  test('Response should return the created super admin', async () => {
+    const response = await request(app).post('/super-admin').send({
+      firstName: 'Risotto',
+      lastName: 'Nero',
+      password: 'M3t4ll1c4',
+      role: 'SA',
+      email: 'risotto.n@squadra.com',
+      active: false,
+    });
+    expect(response.body.data).not.toBe(undefined);
+    superAdminId3 = response.body.data._id;
+  });
+});
+
+describe('GET /super-admins/:id', () => {
+  test('Response should return a 200 status', async () => {
+    const response = await request(app).get(`/super-admin/${superAdminId1}`).send();
+    expect(response.status).toBe(200);
+  });
+
+  test('Response should return error false', async () => {
+    const response = await request(app).get(`/super-admin/${superAdminId1}`).send();
+    expect(response.error).toBe(false);
+  });
+
+  test('Response should return a super admin', async () => {
+    const response = await request(app).get(`/super-admin/${superAdminId1}`).send();
+    expect(response.body.data).not.toBe(undefined);
+  });
+});
+
+describe('DELETE /super-admins/:id', () => {
+  test('Response should return a 200 status', async () => {
+    const response = await request(app).delete(`/super-admin/${superAdminId1}`).send();
+    expect(response.status).toBe(200);
+  });
+
+  test('Response should return error false', async () => {
+    const response = await request(app).delete(`/super-admin/${superAdminId2}`).send();
+    expect(response.error).toBe(false);
+  });
+});
+
+describe('PUT /super-admins/:id', () => {
+  test('Response should return a 200 status', async () => {
+    const response = await request(app).put(`/super-admin/${superAdminId3}`).send({
+      password: 'p455w0rD',
+      email: 'risotto.nero@squadra.com',
+    });
+    expect(response.status).toBe(200);
+  });
+
+  test('Response should return error false', async () => {
+    const response = await request(app).put(`/super-admin/${superAdminId3}`).send({
+      firstName: 'Fugo',
+      active: true,
+    });
+    expect(response.error).toBe(false);
+  });
+
+  test('Response should return edited super admin', async () => {
+    const response = await request(app).get(`/super-admin/${superAdminId3}`).send({
+      lastName: 'Speedwagon',
+      email: 'pesci@squadra.com',
+    });
+    expect(response.body.data).not.toBe(undefined);
   });
 });
