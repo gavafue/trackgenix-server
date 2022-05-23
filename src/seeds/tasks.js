effdbd87fcb9aa84ded96e057e2baaca97fc3ab8
AlejandroSantini <alejandro.j.santini@gmail.com>
2022-05-23 13:08:26 -0300

    TG-39: Bugs fix for delete tests

--- src/seeds/tasks.js
+++ src/seeds/tasks.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export default [
   {
-    id: mongoose.Types.ObjectId('628ab5b2fc13ae2a48000064'),
+    _id: mongoose.Types.ObjectId('628ab5b2fc13ae2a48000064'),
     nameProject: '62812abf640999a5ff4d0f48',
     week: 1,
     day: 8,
@@ -10,7 +10,7 @@ export default [
     hours: 5,
   },
   {
-    id: mongoose.Types.ObjectId('628ab5b2fc13ae2a48000065'),
+    _id: mongoose.Types.ObjectId('628ab5b2fc13ae2a48000065'),
     nameProject: '62812abf640999a5ff4d0f47',
     week: 3,
     day: 9,
@@ -18,7 +18,7 @@ export default [
     hours: 9,
   },
   {
-    id: mongoose.Types.ObjectId('628ab5b2fc13ae2a48000066'),
+    _id: mongoose.Types.ObjectId('628ab5b2fc13ae2a48000066'),
     nameProject: '62812abf640999a5ff4d0f46',
     week: 2,
     day: 7,
@@ -26,7 +26,7 @@ export default [
     hours: 8,
   },
   {
-    id: mongoose.Types.ObjectId('628ab5b2fc13ae2a48000067'),
+    _id: mongoose.Types.ObjectId('628ab5b2fc13ae2a48000067'),
     nameProject: '62812abf640999a5ff4d0f48',
     week: 4,
     day: 6,
@@ -34,7 +34,7 @@ export default [
     hours: 6,
   },
   {
-    id: mongoose.Types.ObjectId('628ab5b2fc13ae2a48000068'),
+    _id: mongoose.Types.ObjectId('628ab5b2fc13ae2a48000068'),
     nameProject: '62812abf640999a5ff4d0f46',
     week: 2,
     day: 4,
--- src/tests/tasks.test.js
+++ src/tests/tasks.test.js
@@ -9,7 +9,10 @@ beforeAll(async () => {
   await Tasks.collection.insertMany(projectsSeed);
 });
 
-// let taskId;
+let taskId1;
+let taskId2;
+let taskId3;
+let taskId4;
 
 describe('GET /tasks', () => {
   test('response should return a 200 status', async () => {
@@ -30,28 +33,35 @@ describe('GET /tasks', () => {
   test('response should return message for succes', async () => {
     const response = await request(app).get('/tasks').send();
     expect(response.body.message).not.toEqual('Tasks was not found');
-    // taskId = response.body.data.id;
   });
 });
 
-// describe('DELETE /task:id', () => {
-//   test('response should return a 200 status', async () => {
-//     const response = await request(app).delete(`/tasks/${taskId}`).send();
-//     expect(response.status).toBe(200);
-//   });
+describe('DELETE /task:id', () => {
+  test('response should return a 200 status', async () => {
+    // eslint-disable-next-line no-underscore-dangle
+    taskId1 = tasksSeed[0]._id;
+    const response = await request(app).delete(`/tasks/${taskId1}`).send();
+    expect(response.status).toBe(200);
+  });
 
-//   test('response should return false error', async () => {
-//     const response = await request(app).delete(`/tasks/${taskId}`).send();
-//     expect(response.error).toBe(false);
-//   });
+  test('response should return false error', async () => {
+    // eslint-disable-next-line no-underscore-dangle
+    taskId2 = tasksSeed[1]._id;
+    const response = await request(app).delete(`/tasks/${taskId2}`).send();
+    expect(response.error).toBe(false);
+  });
 
-//   test('response should return message for succes', async () => {
-//     const response = await request(app).delete(`/tasks/${taskId}`).send();
-//     expect(response.body.message).toEqual(`Task with ID ${taskId} deleted.`);
-//   });
+  test('response should return message for succes', async () => {
+    // eslint-disable-next-line no-underscore-dangle
+    taskId3 = tasksSeed[2]._id;
+    const response = await request(app).delete(`/tasks/${taskId3}`).send();
+    expect(response.body.message).toEqual(`Task with ID ${taskId3} deleted.`);
+  });
 
-//   test('response should return message for succes', async () => {
-//     const response = await request(app).delete(`/tasks/${taskId}`).send();
-//     expect(response.body.message).not.toEqual(`The task with ID ${taskId} can't be found.`);
-//   });
-// });
+  test('response should return message for succes', async () => {
+    // eslint-disable-next-line no-underscore-dangle
+    taskId4 = tasksSeed[3]._id;
+    const response = await request(app).delete(`/tasks/${taskId4}`).send();
+    expect(response.body.message).not.toEqual(`The task with ID ${taskId4} can't be found.`);
+  });
+});
